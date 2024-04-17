de74196598d4efa76bffb5f44f2b3e827c4730f9
Jahid Hasan <jh3495689@gmail.com>
2024-04-17 19:27:26 +0600

    added mode functinality

--- src/pages/home/Header.jsx
+++ src/pages/home/Header.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+import { CiDark, CiLight, CiSettings } from "react-icons/ci";
 import modeIcon from "../../assets/icons/mode.png";
 import searchIcon from "../../assets/icons/search.png";
 
 const Header = () => {
+  const [mode, setMode] = useState(false);
+  const outSideModeRef = useRef(null)
+
+  const handleClickOutside = (event) => {
+    if(outSideModeRef.current && !outSideModeRef.current.contains(event.target)){
+      setMode(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  },[])
   return (
-    <div className="flex justify-between items-center">
+    <div ref={outSideModeRef} className="flex justify-between items-center">
       <div className="relative">
         <img className="absolute top-1/3 left-5" src={searchIcon} alt="" />
         <input
@@ -13,8 +31,26 @@ const Header = () => {
           placeholder="Search...."
         />
       </div>
+
+      {/* TODO: SHADOW OR ICON FIXED */}
       <div className="flex items-center gap-4">
-        <img src={modeIcon} alt="" />
+        <div className="relative">
+        <img onClick={() => setMode(!mode)} className="cursor-pointer" src={modeIcon} alt="" />
+          <div className={`${mode ? 'grid' : 'hidden'} absolute -right-4 top-10 bg-white gap-[2px] border rounded-lg w-[184px]`}>
+          <div className="flex gap-[10px] items-center hover:bg-secondary hover:text-primary cursor-pointer p-2">
+            <CiLight className="text-2xl" />
+            <h3 className="leading-[18.2px] text-sm">Light</h3>
+          </div>
+          <div className="flex gap-[10px] items-center hover:bg-secondary hover:text-primary cursor-pointer p-2">
+            <CiDark className="text-2xl" />
+            <h3 className="leading-[18.2px] text-sm">Dark</h3>
+          </div>
+          <div className="flex gap-[10px] items-center hover:bg-secondary hover:text-primary cursor-pointer p-2">
+            <CiSettings className="text-2xl" />
+            <h3 className="leading-[18.2px] text-sm">System</h3>
+          </div>
+        </div>
+        </div>
         <button className="px-4 py-[14px] border border-secondary rounded-2xl font-semibold text-primary">
           Log in
         </button>
