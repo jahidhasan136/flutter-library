583082f7f19ae0b5074618a312608247307df1cc
Jahid Hasan <jh3495689@gmail.com>
2024-04-20 02:01:54 +0600

    header section complete

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
+// icons
 import modeIcon from '../assets/icons/mode.png';
 import searchIcon from '../assets/icons/search.png';
 import lightIcon from '../assets/icons/light.svg';
 import darkIcon from '../assets/icons/dark.svg';
 import systemIcon from '../assets/icons/systems.svg';
+import userIcon from '../assets/icons/user.svg';
+import getProIcon from '../assets/icons/get-pro.svg';
+import collectionsIcon from '../assets/icons/collections.svg';
 
 const Header = () => {
   // dark mode light mode implement
   const [mode, setMode] = useState(false);
+  const [user, setUser] = useState(false);
   const outSideModeRef = useRef(null);
 
   // when i cick outside of card mode card are invisible
@@ -17,6 +22,7 @@ const Header = () => {
       !outSideModeRef.current.contains(event.target)
     ) {
       setMode(false);
+      setUser(false);
     }
   };
   useEffect(() => {
@@ -33,7 +39,7 @@ const Header = () => {
         <img className="absolute top-1/3 left-5" src={searchIcon} alt="" />
         <input
           className="py-[14px] pl-11
-           pr-5 w-[400px] rounded-full bg-secondary text-textSecondary"
+           pr-5 w-[400px] rounded-full bg-bgSecondary text-textSecondary"
           type="text"
           placeholder="Search...."
         />
@@ -48,20 +54,21 @@ const Header = () => {
             src={modeIcon}
             alt=""
           />
+          {/* mode modal */}
           <div
             className={`${
               mode ? 'grid' : 'hidden'
             } absolute -right-4 top-10 gap-[2px] rounded-lg w-[184px] p-2 shadow-shadowMd`}
           >
-            <div className="flex gap-[10px] items-center hover:bg-secondary hover:text-primary cursor-pointer p-1 rounded-lg">
+            <div className="flex gap-[10px] items-center hover:bg-bgSecondary hover:text-primary cursor-pointer p-1 rounded-lg">
               <img src={lightIcon} alt="" />
               <h3 className="leading-[18.2px] text-sm">Light</h3>
             </div>
-            <div className="flex gap-[10px] items-center hover:bg-secondary hover:text-primary cursor-pointer p-1 rounded-lg">
+            <div className="flex gap-[10px] items-center hover:bg-bgSecondary hover:text-primary cursor-pointer p-1 rounded-lg">
               <img src={darkIcon} alt="" />
               <h3 className="leading-[18.2px] text-sm">Dark</h3>
             </div>
-            <div className="flex gap-[10px] items-center hover:bg-secondary hover:text-primary cursor-pointer p-1 rounded-lg">
+            <div className="flex gap-[10px] items-center hover:bg-bgSecondary hover:text-primary cursor-pointer p-1 rounded-lg">
               <img src={systemIcon} alt="" />
               <h3 className="leading-[18.2px] text-sm">System</h3>
             </div>
@@ -69,6 +76,42 @@ const Header = () => {
         </div>
         <button className="btn_primary_s">Log in</button>
         <button className="btn_primary_a">Get Pro</button>
+        <div className="relative">
+          <button onClick={() => setUser(!user)} className="btn_primary_s">
+            <img src={userIcon} alt="" />
+          </button>
+          {/* user modal */}
+          <div
+            className={`${
+              user ? 'grid gap-3' : 'hidden'
+            } absolute right-2 top-20 gap-[2px] rounded-lg w-[184px] px-2 py-4 shadow-shadowMd`}
+          >
+            <div className="grid gap-1">
+              <h7 className="text-h7 text-primary  font-medium">
+                Abu Anwar MD Abdullah
+              </h7>
+              <p className="text-h7 text-textSecondary">
+                abuanwar072@gmail.com
+              </p>
+              <div className="border-b border-secondary"></div>
+              <div className="grid gap-1">
+                <button className="flex gap-2 items-center hover:bg-bgSecondary hover:text-primary p-1 rounded-lg">
+                  <img src={getProIcon} alt="" />
+                  <h6 className="text-h6 text-primary capitalize">Get Pro</h6>
+                </button>
+                <button className="flex gap-2 items-center hover:bg-bgSecondary hover:text-primary p-1 rounded-lg">
+                  <img src={collectionsIcon} alt="" />
+                  <h6 className="text-h6 text-primary capitalize">
+                    Collections
+                  </h6>
+                </button>
+                <button className="text-h6 text-primary text-start hover:bg-bgSecondary hover:text-primary p-1 rounded-lg">
+                  Log out
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
