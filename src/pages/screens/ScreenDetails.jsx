4988c29b0316fadda1771bc0d11c05e2c8b26a61
Jahid Hasan <jh3495689@gmail.com>
2024-04-21 20:07:49 +0600

    added screens details page

--- src/pages/screens/ScreenDetails.jsx
+++ src/pages/screens/ScreenDetails.jsx
@@ -25,9 +25,9 @@ const ScreenDetails = ({ copied, handleCopyTextChange, handleCopyText }) => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <div className="p-5 w-[539px] min-h-full bg-white grid gap-4 items-center">
+        <div className="p-5 w-[539px] min-h-full bg-white dark:bg-dark grid gap-4 items-center">
           <div className="flex items-center justify-between">
-            <h3 className="text-h3">Sign in page</h3>
+            <h3 className="text-h3 dark:text-secondary">Sign in page</h3>
             <img
               className="cursor-pointer"
               src={closeIcon}
@@ -48,15 +48,19 @@ const ScreenDetails = ({ copied, handleCopyTextChange, handleCopyText }) => {
               <div
                 className={`${
                   barModalOpen ? 'grid gap-1' : 'hidden'
-                } absolute mt-4 right-0 z-20 w-[184px] shadow-shadowMd p-2 bg-white rounded-lg`}
+                } absolute mt-4 right-0 z-20 w-[184px] shadow-shadowMd p-2 bg-white dark:bg-[#1E1E1E] rounded-lg`}
               >
-                <div className="flex items-center gap-2 hover:bg-bgSecondary hover:text-textPrimary cursor-pointer p-1 rounded-lg">
+                <div className="flex items-center gap-2 hover:bg-bgSecondary hover:text-textPrimary cursor-pointer p-1 rounded-lg dark:hover:bg-darkSecondary dark:hover:text-secondary">
                   <img src={collectionIcon} alt="" />
-                  <p className="text-h6 text-textPrimary">Save</p>
+                  <p className="text-h6 text-textPrimary  dark:text-secondary">
+                    Save
+                  </p>
                 </div>
-                <div className="flex items-center gap-2 hover:bg-bgSecondary hover:text-textPrimary cursor-pointer p-1 rounded-lg">
+                <div className="flex items-center gap-2 hover:bg-bgSecondary hover:text-textPrimary cursor-pointer p-1 rounded-lg dark:hover:bg-darkSecondary dark:hover:text-secondary">
                   <img src={downloadIcon} alt="" />
-                  <p className="text-h6 text-textPrimary">Download code</p>
+                  <p className="text-h6 text-textPrimary  dark:text-secondary">
+                    Download code
+                  </p>
                 </div>
               </div>
             </div>
@@ -67,15 +71,15 @@ const ScreenDetails = ({ copied, handleCopyTextChange, handleCopyText }) => {
 
           {/* tags */}
           <div className="flex  gap-2">
-            <h5 className="text-h5">Tags:</h5>
+            <h5 className="text-h5 dark:text-secondary">Tags:</h5>
             <div className="flex gap-1">
-              <p className="text-textSecondary bg-secondary px-2 py-1 text-h7 rounded-lg hover:text-textPrimary duration-300">
+              <p className="text-textSecondary bg-secondary px-2 py-1 text-h7 rounded-lg hover:text-textPrimary duration-300 dark:bg-transparent dark:border dark:border-darkSecondary dark:hover:text-secondary">
                 Auth
               </p>
-              <p className="text-textSecondary bg-secondary px-2 py-1 text-h7 rounded-lg hover:text-textPrimary duration-300">
+              <p className="text-textSecondary bg-secondary px-2 py-1 text-h7 rounded-lg hover:text-textPrimary duration-300 dark:bg-transparent dark:border dark:border-darkSecondary dark:hover:text-secondary">
                 Sign in
               </p>
-              <p className="text-textSecondary bg-secondary px-2 py-1 text-h7 rounded-lg hover:text-textPrimary duration-300">
+              <p className="text-textSecondary bg-secondary px-2 py-1 text-h7 rounded-lg hover:text-textPrimary duration-300 dark:bg-transparent dark:border dark:border-darkSecondary dark:hover:text-secondary">
                 Register
               </p>
             </div>
@@ -86,7 +90,9 @@ const ScreenDetails = ({ copied, handleCopyTextChange, handleCopyText }) => {
               <p className="text-textSecondary text-h7">
                 This screen is part of
               </p>
-              <h3 className="text-textPrimary text-h5">Shoplo-Shop Template</h3>
+              <h3 className="text-textPrimary text-h5 dark:text-secondary">
+                Shoplo-Shop Template
+              </h3>
               <button className="btn_primary_d">View full template</button>
             </div>
           </div>
