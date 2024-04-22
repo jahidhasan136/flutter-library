e05270e6fa76823f1f66b113d2ee1f0ae5b5553f
Jahid Hasan <jh3495689@gmail.com>
2024-04-22 14:56:12 +0600

    all screens se to scroll in y

--- src/pages/home/SideMenu.jsx
+++ src/pages/home/SideMenu.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import articlesIcon from '../../assets/icons/articleIcon.svg';
-import discoverIcon from '../../assets/icons/discoverIcon.svg';
 import hireIcon from '../../assets/icons/hireIcon.svg';
 import screensIcon from '../../assets/icons/screenIcon.svg';
 import sponsoreIcon from '../../assets/icons/sponsorIcon.svg';
@@ -70,7 +69,79 @@ const SideMenu = () => {
           <h4 className="text-h6 text-textPrimary pl-4 pb-2 dark:text-white">
             All Screens
           </h4>
-          <div className="grid gap-2 text-textSecondary">
+          <div className="grid gap-2 text-textSecondary h-[60%] overflow-y-auto">
+            <ActiveLink to="/sign-in">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Sign in</button>
+                <h3 className="text-h6">10</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/chat">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Chat</button>
+                <h3 className="text-h6">5</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/onboarding">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Onboarding</button>
+                <h3 className="text-h6">18</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/e-commerce">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">E-commerce</button>
+                <h3 className="text-h6">9</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/checkout">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Checkout</button>
+                <h3 className="text-h6">10</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/error">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Error</button>
+                <h3 className="text-h6">16</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/sign-in">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Sign in</button>
+                <h3 className="text-h6">10</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/chat">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Chat</button>
+                <h3 className="text-h6">5</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/onboarding">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Onboarding</button>
+                <h3 className="text-h6">18</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/e-commerce">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">E-commerce</button>
+                <h3 className="text-h6">9</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/checkout">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Checkout</button>
+                <h3 className="text-h6">10</h3>
+              </div>
+            </ActiveLink>
+            <ActiveLink to="/error">
+              <div className="flex justify-between py-2 pl-6 pr-4 items-center">
+                <button className="text-h6">Error</button>
+                <h3 className="text-h6">16</h3>
+              </div>
+            </ActiveLink>
             <ActiveLink to="/sign-in">
               <div className="flex justify-between py-2 pl-6 pr-4 items-center">
                 <button className="text-h6">Sign in</button>
